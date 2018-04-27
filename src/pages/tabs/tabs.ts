7ab10adcc0358dd7928dee9252d0107bdc7c8853
GustavoTolentinos <gustavo.hassen@tolentinos.com>
2018-04-27 14:47:17 -0300

    first commit

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,11 @@
+import { IntroPage } from './../intro/intro';
+import { FeedPage } from './../feed/feed';
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,8 +13,8 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab4Root = FeedPage;
+  tab5Root = IntroPage;
 
   constructor() {
 
